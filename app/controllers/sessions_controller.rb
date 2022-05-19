7a2fbdfe8473eca6cc5092e8517e99ad8b669b11
abhibhandari1 <abhishek.bhandari@kellogg.northwestern.edu>
2022-05-19 02:55:58 +0000

    -

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+    def new
+    end
+    def create
+        @user = User.find_by({"email" => params["email"] })
+        if @user
+            if BCrypt::Password.new(@user["password"]) == params["password"]
+                # cookies["monster"] = "Me like cookies"
+                session["user_id"] = @user["id"]
+                flash["notice"] = "Welcome"
+                redirect_to "/companies"
+            else
+                flash["notice"] = "Nope"
+                redirect_to "/sessions/new"
+            end
+        else
+        redirect_to "/sessions/new"
+        end
+        def destroy
+            session["user_id"] = nil
+            flash["notice"] = "Goodbye"
+            redirect_to "/sessions/new"
+        end
+
+
+    end
+
+end
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,7 +2,7 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
-  # :passw, # TODO: comment back in to hide params beginning with "passw"
+  :passw, # TODO: comment back in to hide params beginning with "passw"
   :secret,
   :token,
   :_key,
@@ -10,5 +10,6 @@ Rails.application.config.filter_parameters += [
   :salt,
   :certificate,
   :otp,
-  :ssn
+  :ssn,
+  :creditcard
 ]
